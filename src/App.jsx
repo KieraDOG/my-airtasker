e0325df03d45ec81cfc28a2a91ed304baae3081e
Long Zhao <zlong@outlook.com>
2020-10-11 17:10:38 +1100

    ✨ Introduce Guard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Guard from './components/Guard';
 import Header from './components/Header';
 import Router, { Route } from './components/Router';
 import { Authentication } from './components/withAuthentication';
@@ -31,7 +32,14 @@ const App = () => (
         </HeaderWrapper>
         <ContentWrapper>
           <Route path="/" render={() => (<Home />)} />
-          <Route path="/dashboard" render={() => (<Dashboard />)} />
+          <Route
+            path="/dashboard"
+            render={() => (
+              <Guard>
+                <Dashboard />
+              </Guard>
+            )}
+          />
         </ContentWrapper>
         <FooterWrapper>
           Footer
---
+++ src/components/Guard/Guard.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import compose from '../../utils/compose';
+import { withRouter } from '../Router';
+import withAuthentication from '../withAuthentication';
+
+class Guard extends React.Component {
+  componentDidMount() {
+    this.preCheck();
+  }
+
+  preCheck() {
+    const { authentication, router } = this.props;
+
+    if (!authentication.user) {
+      router.push('/');
+    }
+  }
+
+  render() {
+    const { authentication, children } = this.props;
+
+    if (!authentication.user) {
+      return null;
+    }
+
+    return children;
+  }
+}
+
+Guard.propTypes = {
+  authentication: PropTypes.shape({
+    user: PropTypes.shape({}),
+  }).isRequired,
+  router: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const EnhancedGuard = compose(
+  withAuthentication,
+  withRouter,
+)(Guard);
+
+export default EnhancedGuard;
